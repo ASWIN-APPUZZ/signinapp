9b9615565ea895e957534f5db457ce9fcd160909
aswin <aswin0484@gmail.com>
2023-09-04 14:26:47 +0530

    initial commit

--- signinapp/ViewController.swift
+++ signinapp/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PasswordTxt: UITextField!
+    @IBOutlet weak var EmailTxt: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btn_Tapped(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        vc.email = EmailTxt.text!
+        vc.password = PasswordTxt.text!
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
 
